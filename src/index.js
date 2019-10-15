94d51918045bbfaeb94f9ac43a78f0f44614c29d
Serhii.Shramko <shramko.web@yahoo.com>
2019-10-15 21:36:30 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/app/app';
+
+const rootElement = document.querySelector(`#root`);
 
 const init = () => {
   ReactDOM.render(
-    <App/>,
-    document.querySelector(`#root`)
+      <App/>,
+      rootElement
   );
 };
 
