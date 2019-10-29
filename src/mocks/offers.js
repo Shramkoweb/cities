adf306299c19042eb65123b903a0c22a0a7796d9
serhii.shramko <shramko.web@yahoo.com>
2019-10-29 18:39:45 +0200

    update mocks data

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -1,42 +1,50 @@
 export const OFFERS = [
   {
     id: 834576,
+    city: `Amsterdam`,
     title: `Beautiful & luxurious apartment at great location`,
     previewPhoto: `img/apartment-01.jpg`,
     isPremium: true,
     isFavorite: true,
     price: 120,
     rating: 43,
-    type: `Apartment`
+    type: `Apartment`,
+    coordinates: [52.3909553943508, 4.85309666406198]
   },
   {
     id: 786198,
+    city: `Amsterdam`,
     title: `Wood and stone place`,
     previewPhoto: `img/apartment-02.jpg`,
     price: 80,
     isPremium: true,
     isFavorite: false,
     rating: 12,
-    type: `Private room`
+    type: `Private room`,
+    coordinates: [52.369553943508, 4.85309666406198]
   },
   {
     id: 123072,
+    city: `Amsterdam`,
     title: `Canal View Prinsengracht`,
     previewPhoto: `img/apartment-03.jpg`,
     price: 132,
     isPremium: false,
     isFavorite: true,
     rating: 66,
-    type: `Bungalow`
+    type: `Bungalow`,
+    coordinates: [52.3909553943508, 4.929309666406198]
   },
   {
     id: 788764,
+    city: `Amsterdam`,
     title: `Nice, cozy, warm big bed apartment`,
     previewPhoto: `img/room.jpg`,
     price: 180,
     isPremium: false,
     isFavorite: false,
     rating: 100,
-    type: `Apartment`
+    type: `Apartment`,
+    coordinates: [52.3809553943508, 4.939309666406198]
   },
 ];
