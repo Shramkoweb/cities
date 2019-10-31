c721c1013b04b3ef3d5079cdc09e8624dfa05b2c
serhii.shramko <shramko.web@yahoo.com>
2019-10-31 16:21:37 +0200

    add Sort component

--- src/components/catalog/catalog.jsx
+++ src/components/catalog/catalog.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import PlacesList from "../places-list/places-list";
 import CatalogEmpty from "../catalog-empty/catalog-empty";
 import Tabs from "../tabs/tabs";
+import Sort from "../sort/sort";
 
 const Catalog = (props) => {
   const {offers, citiesCoordinates} = props;
@@ -20,20 +21,8 @@ const Catalog = (props) => {
               <section className="cities__places places">
                 <h2 className="visually-hidden">Places</h2>
                 <b className="places__found">312 places to stay in Amsterdam</b>
-                <form className="places__sorting" action="#" method="get">
-                  <span className="places__sorting-caption">Sort by</span>
-                  <span className="places__sorting-type" tabIndex="0">Popular
-                    <svg className="places__sorting-arrow" width="7" height="4">
-                      <use xlinkHref="#icon-arrow-select"/>
-                    </svg>
-                  </span>
-                  <ul className="places__options places__options--custom">
-                    <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                    <li className="places__option" tabIndex="0">Price: low to high</li>
-                    <li className="places__option" tabIndex="0">Price: high to low</li>
-                    <li className="places__option" tabIndex="0">Top rated first</li>
-                  </ul>
-                </form>
+
+                <Sort/>
 
                 <PlacesList offers={offers}/>
 
---
+++ src/components/sort/sort.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Sort from "./sort";
+
+it(`Sort component render correct`, () => {
+  const sortTemplate = renderer
+    .create(<Sort/>)
+    .toJSON();
+
+  expect(sortTemplate).toMatchSnapshot();
+});
