3829cba337a519193628edc23b57c3a373ceaa6c
serhii.shramko <shramko.web@yahoo.com>
2019-11-04 22:53:43 +0200

    add tabs-link component

---
+++ src/components/tabs-link/tabs-link.jsx
@@ -0,0 +1,42 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+
+
+export class TabsLink extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(evt) {
+    evt.preventDefault();
+    this.props.changeCity(this.props.city);
+  }
+
+  render() {
+    const {city, currentCity} = this.props;
+    return (
+      <li className="locations__item">
+        <a
+          onClick={this.handleClick}
+          className={`locations__item-link tabs__item ${currentCity === city ? `tabs__item--active` : ``}`}
+          href="#">
+          <span>{city}</span>
+        </a>
+      </li>
+    );
+  }
+}
+
+TabsLink.propTypes = {
+  city: PropTypes.string.isRequired,
+  currentCity: PropTypes.string
+};
+
+const mapStateToProps = (state, ownProps) => Object.assign({}, ownProps, {
+  currentCity: state.city
+});
+
+export default connect(mapStateToProps)(TabsLink);
