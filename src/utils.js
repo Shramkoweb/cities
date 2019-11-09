a61de338b1540870d8da0062ed172fb9025077d2
serhii.shramko <shramko.web@yahoo.com>
2019-11-09 23:18:12 +0200

    refactor: update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 import Constants from "./constants";
 
-const convertFloatToPercentage = (rating) => Number(rating / Constants.MAX_RATING * 100);
+const convertFloatToPercentage = (rating) => {
+  return rating / Constants.MAX_RATING * 100;
+};
 
 export {convertFloatToPercentage};
