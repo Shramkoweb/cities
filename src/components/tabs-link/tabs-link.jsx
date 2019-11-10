9a2976cfa53b6a2a5d075ec927c32f6b5b622822
serhii.shramko <shramko.web@yahoo.com>
2019-11-10 18:01:30 +0200

    refactor: new realisation of tabs-link handlers

--- src/components/tabs-link/tabs-link.jsx
+++ src/components/tabs-link/tabs-link.jsx
@@ -1,37 +1,29 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 
-class TabsLink extends PureComponent {
-  constructor(props) {
-    super(props);
+const TabsLink = (props) => {
+  const {city, currentCity, changeCity} = props;
+  const activeClassName = currentCity === city ? `tabs__item--active` : ``;
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(evt) {
+  const onTabClick = (evt) => {
     evt.preventDefault();
 
-    this.props.changeCity(this.props.city);
-  }
+    changeCity(city);
+  };
 
-  render() {
-    const {city, currentCity} = this.props;
-    const activeClassName = currentCity === city ? `tabs__item--active` : ``;
-
-    return (
-      <li className="locations__item">
-        <a
-          onClick={this.handleClick}
-          className={`locations__item-link tabs__item ${activeClassName}`}
-          href="#"
-        >
-          <span>{city}</span>
-        </a>
-      </li>
-    );
-  }
-}
+  return (
+    <li className="locations__item">
+      <a
+        onClick={onTabClick}
+        className={`locations__item-link tabs__item ${activeClassName}`}
+        href="#"
+      >
+        <span>{city}</span>
+      </a>
+    </li>
+  );
+};
 
 TabsLink.propTypes = {
   city: PropTypes.string.isRequired,
