54ba9b14b4e9c8e762c58ee3143ba37fc4533485
serhii.shramko <shramko.web@yahoo.com>
2019-11-13 18:32:07 +0200

    refactor: remove unused code fragments

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import PropTypes from "prop-types";
-import {connect} from "react-redux";
 import MainPage from "../main-page/main-page";
 
-const App = (props) => {
-  const {offers} = props;
-
+const App = () => {
   return (
-    <MainPage
-      offers={offers}
-    />
+    <MainPage/>
   );
 };
 
-App.propTypes = {
-  offers: PropTypes.arrayOf(PropTypes.object).isRequired
-};
-
-const mapStateToProps = (state) => Object.assign({}, {
-  city: state.city,
-});
-
-export {App};
-export default connect(mapStateToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {reducer} from "./reducer/reducer";
 import App from "./components/app/app";
-import {OFFERS} from "./mocks/offers";
 
 const rootElement = document.querySelector(`#root`);
 const store = createStore(reducer);
@@ -12,7 +11,7 @@ const store = createStore(reducer);
 const init = () => {
   ReactDOM.render(
       <Provider store={store}>
-        <App offers={OFFERS}/>,
+        <App/>,
       </Provider>,
       rootElement
   );
