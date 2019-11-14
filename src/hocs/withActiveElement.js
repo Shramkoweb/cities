63292afd439f83fe766f5fe13eb38426e63af339
serhii.shramko <shramko.web@yahoo.com>
2019-11-14 20:25:52 +0200

    refactor: new realisation of withActiveElement HOC

--- src/hocs/withActiveElement.js
+++ src/hocs/withActiveElement.js
@@ -4,24 +4,27 @@ const withActiveElement = (Component) => {
   class WithActiveElement extends React.PureComponent {
     constructor(props) {
       super(props);
-      this.state = {active: null};
+      this.state = {activeElement: null};
 
       this.elementSelectHandler = this.elementSelectHandler.bind(this);
     }
 
     elementSelectHandler(id) {
       this.setState({
-        active: id
+        activeElement: id
       });
     }
 
     render() {
       return (
-        <Component
-          {...this.props}
-          active={this.state.active}
-          onSelect={this.elementSelectHandler}
-        />
+        <>
+          <Component
+            {...this.props}
+            activeElement={this.state.activeElement}
+            onSelect={this.elementSelectHandler}
+          />
+          <div>Active: {this.state.activeElement}</div>
+        </>
       );
     }
   }
