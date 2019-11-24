ef195cce1ae23b69e0da68eb96a6a4fb622d5d25
serhii.shramko <shramko.web@yahoo.com>
2019-11-24 12:33:04 +0200

    build: add reselect

---
+++ src/reducer/data/selector.js
@@ -0,0 +1,30 @@
+import {createSelector} from "reselect";
+
+import NameSpace from "../name-spaces";
+
+const NAME_SPACE = NameSpace.DATA;
+
+const getActiveCity = (state) => {
+  return state[NAME_SPACE].city;
+};
+
+const getCities = (state) => {
+  return [...new Set(state[NAME_SPACE].offers.map((item) => item.city.name))].sort();
+};
+
+const getOffers = (state) => {
+  return state[NAME_SPACE].offers;
+};
+
+const getFilteredOffers = createSelector(
+    getActiveCity,
+    getOffers,
+    (offers, city) => offers.filter((offer) => offer.city.name === city)
+);
+
+export {
+  getActiveCity,
+  getCities,
+  getFilteredOffers,
+  getOffers,
+};
