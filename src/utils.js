77394d2d9e64eb3b4729c898668a28e9bc2d9d24
serhii.shramko <shramko.web@yahoo.com>
2019-11-24 22:00:21 +0200

    style: remove unused code

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,6 @@ const convertFloatToPercentage = (rating) => {
   return rating / Constants.MAX_RATING * 100;
 };
 
-const getOffersByCity = (cityOffers, city) => { // TODO временно убрал сюда
-  return cityOffers.filter((item) => item.city === city);
-};
-
 export {
-  convertFloatToPercentage,
-  getOffersByCity
+  convertFloatToPercentage
 };
