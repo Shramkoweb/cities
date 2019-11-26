6d1f369f869cd94c61266053c21c6875b9fd52b4
serhii.shramko <shramko.web@yahoo.com>
2019-11-26 20:55:19 +0200

    refactor: new realisation of PlaceCardAdapter

--- src/adapters/place-card-adapter.js
+++ src/adapters/place-card-adapter.js
@@ -1,33 +1,33 @@
 class PlaceCardAdapter {
   static parseOffer(offer) {
     return {
-      id: offer.id,
+      id: offer[`id`],
       city: {
-        name: offer.city.name,
-        location: [offer.city.location.latitude, offer.city.location.longitude],
-        zoom: offer.city.zoom,
+        name: offer[`city`][`name`],
+        location: [offer[`city`][`location`][`latitude`], offer[`city`][`location`][`longitude`]],
+        zoom: offer[`city`][`zoom`],
       },
       previewImage: offer[`preview_image`],
-      images: offer.images,
-      title: offer.title,
+      images: offer[`images`],
+      title: offer[`title`],
       isFavorite: offer[`is_favorite`],
       isPremium: offer[`is_premium`],
-      rating: offer.rating,
-      type: offer.type,
-      bedrooms: offer.bedrooms,
+      rating: offer[`rating`],
+      type: offer[`type`],
+      bedrooms: offer[`bedrooms`],
       maxAdults: offer[`max_adults`],
-      price: offer.price,
-      goods: offer.goods,
+      price: offer[`price`],
+      goods: offer[`goods`],
       host: {
-        id: offer.host.id,
-        name: offer.host.name,
-        isPro: offer.host[`is_pro`],
-        avatar: offer.host[`avatar_url`]
+        id: offer[`host`][`id`],
+        name: offer[`host`][`name`],
+        isPro: offer[`host`][`is_pro`],
+        avatar: offer[`host`][`avatar_url`]
       },
-      description: offer.description,
+      description: offer[`description`],
       location: {
-        coordinates: [offer.location.latitude, offer.location.longitude],
-        zoom: offer.location.zoom
+        coordinates: [offer[`location`][`latitude`], offer[`location`][`longitude`]],
+        zoom: offer[`location`][`zoom`]
       }
     };
   }
