c93cb620271325413a1482702b07b95d8b4005bd
serhii.shramko <shramko.web@yahoo.com>
2019-12-01 15:31:01 +0200

    add witSignForm HOC

---
+++ src/hocs/with-sign-form/wit-sign-form.js
@@ -0,0 +1,39 @@
+import React, {PureComponent} from "react";
+
+export const witSignForm = (Component) => {
+  class WithSignForm extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        email: ``,
+        password: ``,
+      };
+
+      this._handleChangeEmail = this._handleChangeEmail.bind(this);
+      this._handleChangePassword = this._handleChangePassword.bind(this);
+    }
+
+    _handleChangeEmail(evt) {
+      this.setState({email: evt.target.value});
+    }
+
+    _handleChangePassword(evt) {
+      this.setState({password: evt.target.value});
+    }
+
+    render() {
+      return (
+        <Component
+          {...this.props}
+          onChangeEmail={this._handleChangeEmail}
+          onChangePassword={this._handleChangePassword}
+          emailValue={this.state.email}
+          passwordValue={this.state.password}
+        />
+      );
+    }
+  }
+
+  return WithSignForm;
+};
