2cf64608084d8064d0b8aecc3982a55ef4fdae74
serhii.shramko <shramko.web@yahoo.com>
2019-12-01 15:30:42 +0200

    refactor: new realisation of user reducer

--- src/reducer/user/selector.js
+++ src/reducer/user/selector.js
@@ -2,6 +2,15 @@ import NameSpace from "../name-spaces";
 
 const NAME_SPACE = NameSpace.USER;
 
-export const getAuthorizationStatus = (state) => {
+const getAuthorizationStatus = (state) => {
   return state[NAME_SPACE].isAuthorizationRequired;
+};
+
+const getUserData = (state) => {
+  return state[NAME_SPACE].userData;
+};
+
+export {
+  getAuthorizationStatus,
+  getUserData,
 };
--- src/reducer/user/user.js
+++ src/reducer/user/user.js
@@ -1,24 +1,44 @@
+import {REQUEST_URL} from "../../constants";
+
 const initialState = {
   isAuthorizationRequired: true,
+  userData: {},
 };
 
 const Action = {
   REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+  AUTHORIZATION: `AUTHORIZATION`,
 };
 
 const ActionCreator = {
   requireAuthorization: (status) => ({
     type: Action.REQUIRED_AUTHORIZATION,
     payload: status
-  })
+  }),
+  authorization: (userData) => ({
+    type: Action.AUTHORIZATION,
+    payload: userData
+  }),
+};
+
+const Operation = {
+  fetchAuthData: (authData) => (dispatch, _, api) => {
+    return api.post(REQUEST_URL.LOGIN, authData)
+      .then(({data}) => {
+        dispatch(ActionCreator.authorization(data));
+        dispatch(ActionCreator.requireAuthorization(false));
+      });
+  }
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case Action.REQUIRED_AUTHORIZATION:
       return Object.assign({}, state, {isAuthorizationRequired: action.payload});
+    case Action.AUTHORIZATION:
+      return Object.assign({}, state, {userData: action.payload});
   }
   return state;
 };
 
-export {ActionCreator, Action, reducer};
+export {ActionCreator, Action, reducer, Operation};
