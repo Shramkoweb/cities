384120f4cd5b3aa03cc89630c5add0527d5029cc
serhii.shramko <shramko.web@yahoo.com>
2019-12-11 18:22:53 +0200

    add sort Component

--- src/components/sort/sort.jsx
+++ src/components/sort/sort.jsx
@@ -1,22 +1,62 @@
 import React from "react";
+import {connect} from "react-redux";
+import Option from "../option/option";
+import {SortOptions} from "../../constants";
+import {ActionCreator} from "../../reducer/data/data";
+import PropTypes from "prop-types";
 
-const Sort = () => {
-  return (
-    <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by </span>
-      <span className="places__sorting-type" tabIndex="0">Popular
-        <svg className="places__sorting-arrow" width="7" height="4">
-          <use xlinkHref="#icon-arrow-select"/>
-        </svg>
-      </span>
-      <ul className="places__options places__options--custom">
-        <li className="places__option places__option--active" tabIndex="0">Popular</li>
-        <li className="places__option" tabIndex="0">Price: low to high</li>
-        <li className="places__option" tabIndex="0">Price: high to low</li>
-        <li className="places__option" tabIndex="0">Top rated first</li>
-      </ul>
-    </form>
-  );
+class Sort extends React.PureComponent {
+  render() {
+    const {
+      currentSortType,
+      isOpen,
+      labelName,
+      onOpenSorting,
+      onSelectOption,
+      onSetTypeSort,
+    } = this.props;
+
+    return (
+      <>
+        <span
+          onClick={onOpenSorting}
+          className="places__sorting-type"
+          tabIndex="0"
+        >
+          {labelName}
+          <svg className="places__sorting-arrow" width="7" height="4">
+            <use xlinkHref="#icon-arrow-select"/>
+          </svg>
+        </span>
+        <ul className={`${isOpen ? `places__options--opened` : ``} places__options places__options--custom`}>
+          {SortOptions.map((option) => {
+            return <Option
+              onSelectOption={onSelectOption}
+              onSetTypeSort={onSetTypeSort}
+              isSelected={currentSortType === option.sortType}
+              sortType={option.sortType}
+              name={option.name}
+              key={`option-${option.sortType}`}
+            />;
+          })}
+        </ul>
+      </>
+    );
+  }
+}
+
+Sort.propTypes = {
+  currentSortType: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  labelName: PropTypes.string.isRequired,
+  onOpenSorting: PropTypes.func.isRequired,
+  onSelectOption: PropTypes.func.isRequired,
+  onSetTypeSort: PropTypes.func.isRequired,
 };
 
-export default Sort;
+const mapDispatchToProps = (dispatch) => ({
+  onSetTypeSort: (value) => dispatch(ActionCreator.setSortType(value)),
+});
+
+export {Sort};
+export default connect(null, mapDispatchToProps)(Sort);
