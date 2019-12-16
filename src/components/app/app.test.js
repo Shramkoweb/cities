5abfd254549b1dfc700d947594054da1fe5f027d
serhii.shramko <shramko.web@yahoo.com>
2019-12-16 20:57:57 +0200

    add app snapshot

---
+++ src/components/app/app.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Enzyme, {render} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+
+import {App} from "./app";
+jest.mock(`../../index.js`, () => jest.fn().mockReturnValue(null));
+
+Enzyme.configure({adapter: new Adapter()});
+
+it(`Is App render`, () => {
+  const tree = render((<App
+    isAuthorizationRequired={false}
+    isLoading={true}
+    isOfferExist={jest.fn()}
+    isError={``}
+  />));
+
+  expect(toJson(tree)).toMatchSnapshot();
+});
